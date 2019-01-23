daf7118ce2e5784c9890286db29776aecfb90769
Jiaxin_Bai <jbai0318@gmail.com>
2019-01-24 02:17:35 +0800

    test data

--- data.py
+++ data.py
@@ -7,7 +7,22 @@ class Data:
         with open(path, "r") as fin:
             self.rawDict = json.load(fin)
 
-        print(self.rawDict)
+        self.id2key = [int(key) for key, value in rawDict.items()]
+        self.key2id = {key: i for i, key in enumerate(self.id2key)}
 
+        self.Xs = np.array([value for key, value in rawDict.items()])
+        print(self.id2key)
+        print(self.key2id)
+        print(self.Xs)
+
+    def get_batch():
+        """
+        get_batch will return a tuple. The first one is the get batch. The second one is the 
+        """
+        pass
+
+
+        
+        
 if __name__ == "__main__":
     data = Data("2016-06-01-2017-06-01all-factors.json")
