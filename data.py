75fa17e2835aa6fafc3c0faab4889d99f8252fe2
Jiaxin_Bai <jbai0318@gmail.com>
2019-01-25 02:30:37 +0800

    hahahha

--- data.py
+++ data.py
@@ -12,19 +12,30 @@ class Data:
 
         self.Xs = np.array([value for key, value in self.rawDict.items()])
        
-    def get_batch():
+    def get_batch(batch_size = 3):
         """
         get_batch will return a tuple. The first is input for encoder and the output of the decoder (with EOS at the end). 
         the second is the input of decoder (with SOS in the front)
         """
 
+
         indices = np.random.randint(Xs.shape[0], size=batch_size)
-        return self.Xs[indices]
+        encoder_input = self.Xs[indices]
+        decoder_input = np.zeros((encoder_input.shape[0], encoder_input.shape[1]+1, encoder_input.shape[2]))
+        decoder_input[:, :-1, :] = encoder_input
+        decoder_label = np.zeros((encoder_input.shape[0], encoder_input.shape[1]+1, encoder_input.shape[2]))
+        decoder_label[:, 1:, :] = encoder_input
+
+        return encoder_input, decoder_input, decoder_label
+
+
+
 
         
 
-
+ 
         
         
 if __name__ == "__main__":
     data = Data("2016-06-01-2017-06-01all-factors.json")
+    print(data.get_batch())
