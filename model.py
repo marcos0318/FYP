4ab1c30db107e830e51e4a5fb257821e8efc6b4d
Jiaxin_Bai <jbai0318@gmail.com>
2019-01-25 03:29:06 +0800

    decoder

--- model.py
+++ model.py
@@ -24,6 +24,9 @@ class Model:
             with tf.variable_scope("decoder"):
                 self.decoder_output, _ = tf.nn.dynamic_rnn(decoder_cell, self.decoder_input, dtype=tf.float32, initial_state = self.encoded_state)
             
+
+            self.decoder_output = tf.reshape(tf.layers.dense(tf.reshape(self.decoder_output, [-1, flags.latent_size]), flags.input_size), [flags.batch_size, -1, flags.input_size])
+
             self.loss = tf.reduce_sum((self.decoder_label - self.decoder_output) * (self.decoder_label - self.decoder_output))
             self.optimizer = tf.train.AdamOptimizer.minimize(self.loss)
 
