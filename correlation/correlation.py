1702fc21541d0ef2d21c495622bf7edd2092dab8
Jiaxin_Bai <jbai0318@gmail.com>
2019-02-12 12:12:37 +0800

    add data

--- correlation/correlation.py
+++ correlation/correlation.py
@@ -7,6 +7,7 @@ rq.init()
 start_date = "2016-01-01"
 end_date = "2018-12-30"
 
-factor_return = rq.get_factor_return(start_date, end_date, factor="all", universe="whole_market", method="explicit")
+factor_return = rq.get_factor_return(start_date, end_date, "all", universe="whole_market", method="explicit")
 
-factor_return.to_csv("factor return")
+factor_return.to_csv("factor_return.csv")
+factor_return.to_json(("factor_return.json"))
